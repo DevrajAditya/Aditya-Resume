39c26e642e8e254e8b15bb8ab554922d2a1bbc9e
Aditya <jhaaditya0101@gmail.com>
2022-08-19 00:03:19 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,6 @@
+// var typed = new typed(".typing", {
+//     strings:["","Web Designer","Wed Developer","Full Stack Developer","Software Developer"],
+//     typespeed:100,
+//     BackSpeed:60,
+//     loop:true
+// })
---
+++ style-switcher.js
@@ -0,0 +1,54 @@
+const styleSwitcherToggler = document.querySelector(".style-switcher-toggler");
+styleSwitcherToggler.addEventListener("click", () => {
+    document.querySelector(".style-switcher").classList.toggle("open");
+})
+
+window.addEventListener("scroll", () =>
+{
+    if(document.querySelector(".style-switcher").classList.contains("open"))
+    {
+        document.querySelector(".style-switcher").classList.remove("open");
+    }
+})
+
+const alternateStyles = document.querySelectorAll(".alternate-style");
+function setActiveStyle(color)
+{
+    alternateStyles.forEach((style) =>
+    {
+        if(color === style.getAttribute("title"))
+        {
+            style.removeAttribute("disabled");
+        }
+        else
+        {
+            style.setAttribute("disabled", "true");
+        }
+    })
+}
+
+const dayNight = document.querySelector(".day-night");
+dayNight.addEventListener("click", () =>
+{
+    dayNight.querySelector("i").classList.toggle("fa-sun");
+    dayNight.querySelector("i").classList.toggle("fa-moon");
+    document.body.classList.toggle("dark");
+})
+window.addEventListener("laod", () =>{
+if(document.body.classList.contains("dark"))
+{
+    dayNight.querySelector("i").classList.add("fa-sun");
+}
+else
+{
+    dayNight.querySelector("i").classList.add("fa-moon");
+}
+})
+
+
+var typed = new typed(".typing", {
+    strings:["","Web Designer","Wed Developer","Full Stack Developer","Software Developer"],
+    typespeed:100,
+    BackSpeed:60,
+    loop:true
+})
